356a26cbf71eb112d0c9c18e4e7c7a4b193bc1b6
Jongchan Kim <ace.jckim@gmail.com>
2020-03-01 09:17:36 +0900

    jongchan week4

---
+++ week4/jongchan/LevelOrderTraversal.java
@@ -0,0 +1,43 @@
+package jcodetest;
+
+import java.util.*;
+
+public class LevelOrderTraversal {
+    public static void main(String args[]) {
+        TreeNode node = new TreeNode(1);
+        leverOrder(node);
+    }
+
+    private static List<List<Integer>> leverOrder(TreeNode node) {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+
+        int inputCountFromLevel = 0;
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+
+            for (int i = 0; i < Math.pow(2, inputCountFromLevel); i++) {
+                TreeNode currentFirst = queue.pop();
+                levelList.add(currentFirst.val);
+                queue.add(currentFirst.left);
+                queue.add(currentFirst.right);
+            }
+            inputCountFromLevel++;
+            result.add(levelList);
+            levelList.clear();
+        }
+
+        return result;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
